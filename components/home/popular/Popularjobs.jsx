65f0b00ef7a487a050691f735f01429655209f65
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-13 20:28:35 +0530

    home page done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,8 +13,6 @@ const Popularjobs = () => {
     num_pages: 1
   })
 
-  console.log(data);
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -30,7 +28,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             renderItem={({ item }) => (
               <PopularJobCard item={item} />
             )}
