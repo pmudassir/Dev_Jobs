ea25847509a57ee9829a34bd0dda878fa7cf36d7
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-14 12:09:32 +0530

    fixed few error

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,6 +13,13 @@ const Popularjobs = () => {
     num_pages: 1
   })
 
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`)
+    setSelectedJob(item.job_id)
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -30,7 +37,11 @@ const Popularjobs = () => {
           <FlatList
             data={data}
             renderItem={({ item }) => (
-              <PopularJobCard item={item} />
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
             )}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
